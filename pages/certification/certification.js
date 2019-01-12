b0dd07ebfa3dd45da2506e6e2338813f70ec7daa
Li Tongxin <tomboy17@163.com>
2019-01-12 10:40:46 +0800

    Uplift certification UI layout.

--- pages/certification/certification.js
+++ pages/certification/certification.js
@@ -1,15 +1,23 @@
 const app = getApp();
+const bgImgWithOrangeBlue = require('../../image/bgImgWithOrangeBlue.js');
+const aclogo = require('../../image/aclogo.js');
 
 Page({
   data: {
     certification: {
-      winner: { name: 'Daniel Ng'},
-      subject: 'design thinking training',
-      awardDate: '12th Dec 2018',
-      expiredDate: '12th Dec 2020', 
+      winner: {
+        firstName: 'John',
+        lastName: 'William'
+      },
+      subject: 'agile coach',
+      awardDate: '3rd Jan 2019',
+      expiredDate: '3rd Jan 2021',
       primaryIssuer: 'ThoughtWorks',
       secondaryIssuers: ['TAL Apparel']
-    } 
+    },
+    hashCode: '0x5c47e30dc7f82167de',
+    logo: aclogo.url,
+    backgroundImage: bgImgWithOrangeBlue.url,
   },
-  onLoad: function () {}
+  onLoad: function() {}
 })
