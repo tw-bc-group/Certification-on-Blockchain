45eb71ee85512c4d0250c619d378681dc2b0de40
RuipengWang <rpwang@99-1-185-113.lightspeed.tukrga.sbcglobal.net>
2019-01-16 18:17:18 +0800

    fix: surround with ' " ' for query -- Ruipeng Wang

--- WeChatMiniProgram/service/bcClient.js
+++ WeChatMiniProgram/service/bcClient.js
@@ -2,7 +2,7 @@ const apiHost = 'https://cac.thoughtworks.cn:3000'
 
 const getCertification = query => {
   return new Promise((resolve, reject) => wx.request({
-    url: apiHost + '/certifications/' + query,
+    url: apiHost + '/certifications/"' + query + '"',
     method: 'GET',
     success: (res) => resolve(res.data)
   }))
@@ -10,7 +10,7 @@ const getCertification = query => {
 
 const getWinner = query => {
   return new Promise((resolve, reject) => wx.request({
-    url: apiHost+ '/winners/' + query,
+    url: apiHost+ '/winners/"' + query + '"',
     method: 'GET',
     success: (res) => resolve(res.data)
   }))
