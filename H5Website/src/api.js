d60928beb2df2a315e2d627dea65d948023e0416
yanqian <qianyan@thoughtworks.com>
2019-01-16 19:12:49 +0800

    fix(quote): add double quotes for query params

--- H5Website/src/api.js
+++ H5Website/src/api.js
@@ -4,10 +4,10 @@ const rootUrl = 'https://cac.thoughtworks.cn:3000'
 
 const actions = {
   getCertification: (query) => axios.get(
-    `${rootUrl}/certifications/${query}`),
+    `${rootUrl}/certifications/"${query}"`),
 
   getWinner: (query) => axios.get(
-    `${rootUrl}/winners/${query}`)
+    `${rootUrl}/winners/"${query}"`)
 }
 
 export default actions
