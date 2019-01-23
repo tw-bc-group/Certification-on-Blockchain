3dc533bcf1526af2857e789a2606b9cea66b5cbb
Li Tongxin <tomboy17@163.com>
2019-01-23 11:31:15 +0800

    Replace other image urls using outer server in MiniProgram.

--- WeChatMiniProgram/pipe/logoGenerator.js
+++ WeChatMiniProgram/pipe/logoGenerator.js
@@ -1,18 +1,18 @@
 const R = require('../3rd/ramda.min.js')
-const logo = require('../image/logo.js')
-const logoCorporate = require('../image/logoCorporate.js')
-const logoCommunity = require('../image/logoCommunity.js')
-const logoUniversity = require('../image/logoUniversity.js')
-const huaweiLogo = require('../image/huaweiLogo.js')
-const githubLogo = require('../image/githubLogo.js')
-const universityLogo = require('../image/universityLogo.js')
+const logo = 'https://upload-images.jianshu.io/upload_images/217988-4ad6312df612c4aa.jpeg?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const logoCorporate = 'https://upload-images.jianshu.io/upload_images/217988-3a9c600958ee2a3c.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const logoCommunity = 'https://upload-images.jianshu.io/upload_images/217988-257c5c5dceba99cd.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const logoUniversity = 'https://upload-images.jianshu.io/upload_images/217988-8a96182cc6ff1c1d.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const huaweiLogo = 'https://upload-images.jianshu.io/upload_images/217988-3275b8cee2460485.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const communityLogo = 'https://upload-images.jianshu.io/upload_images/217988-0b8833beab1ade51.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
+const universityLogo = 'https://upload-images.jianshu.io/upload_images/217988-279c9e609bf72693.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240'
 
 const partnerLogo = R.cond([
   [
     R.equals('Huawei'), R.always(huaweiLogo)
   ],
   [
-    R.equals('Github'), R.always(githubLogo)
+    R.equals('Github'), R.always(communityLogo)
   ],
   [
     R.equals('BeiJingUniversity'), R.always(universityLogo)
@@ -23,23 +23,20 @@ const partnerLogo = R.cond([
 
 
 const genrate = (type, partner) => {
-  return R.compose(
-    R.pluck('url'),
-    R.cond([
-      [
-        R.equals('Corporate'), R.always([logoCorporate, partnerLogo(partner)])
-      ],
-      [
-        R.equals('Community'), R.always([logoCommunity, partnerLogo(partner)])
-      ],
-      [
-        R.equals('University'), R.always([logoUniversity, partnerLogo(partner)])
-      ],
-      [
-        R.T, R.always([logo])
-      ],
-    ])
-  )(type)
+  return R.cond([
+    [
+      R.equals('Corporate'), R.always([logoCorporate, partnerLogo(partner)])
+    ],
+    [
+      R.equals('Community'), R.always([logoCommunity, partnerLogo(partner)])
+    ],
+    [
+      R.equals('University'), R.always([logoUniversity, partnerLogo(partner)])
+    ],
+    [
+      R.T, R.always([logo])
+    ],
+  ])(type)
 }
 
 module.exports = {
