c849c0a7c56b10f9a73ace95b4c62ff35941ccf7
RuipengWang <rpwang@Edward-NoaLand.local>
2019-02-27 10:44:39 +0800

    chore: merge conflict

--- CertificationSmartContract/test/cacCertificationTest.js
+++ CertificationSmartContract/test/cacCertificationTest.js
@@ -1,8 +1,11 @@
+const { getWeb3, getContractInstance } = require("./helper")
+const web3 = getWeb3()
+const getInstance = getContractInstance(web3)
+
 var CACCertificationContract = artifacts.require("./CACCertificationContract.sol");
 
-contract('CACCertificationContract', function(accounts){
-    it("should issue certification successfully", function(accounts){
-        var cacCertification;
+contract('CACCertificationContract', (accounts) => {
+    it("should issue certification successfully", async () => {
         var type = "TW";
         var firstName = "John";
         var lastName = "William";
@@ -12,23 +15,18 @@ contract('CACCertificationContract', function(accounts){
         var expiredDate = new Date("1/12/2020").toLocaleDateString();
         var partner = "Partner";
 
-        return CACCertificationContract.new().then(function(instance){
-            cacCertification = instance;
-            return cacCertification.issueCertification(type, firstName, lastName, mobileNumber, subject, awardDate, expiredDate, partner);
-        }).then(function(){
-            return cacCertification.getCertification(mobileNumber);
-        }).then(function(res){
-            assert.equal(res.valueOf()[0], type);
-            assert.equal(res.valueOf()[1], subject);
-            assert.equal(res.valueOf()[2], awardDate);
-            assert.equal(res.valueOf()[3], expiredDate);
-            assert.equal(res.valueOf()[4], partner);
-        }).then(function(){
-            return cacCertification.getWinner(mobileNumber);
-        }).then(function(res){
-            assert.equal(res.valueOf()[0], firstName);
-            assert.equal(res.valueOf()[1], lastName);
-            assert.equal(web3.utils.hexToUtf8(res.valueOf()[2].valueOf()), "+86 123456789");
-        });
+        const cacCertification = getInstance("CACCertificationContract")
+        await cacCertification.methods.issueCertification(type, firstName, lastName, mobileNumber, subject, awardDate, expiredDate, partner).send({ from: accounts[0] });
+        const res = await cacCertification.methods.getCertification(mobileNumber).call();
+        assert.equal(res.valueOf()[0], type);
+        assert.equal(res.valueOf()[1], subject);
+        assert.equal(res.valueOf()[2], awardDate);
+        assert.equal(res.valueOf()[3], expiredDate);
+        assert.equal(res.valueOf()[4], partner);
+
+        const winner = cacCertification.methods.getWinner(mobileNumber).call();
+        assert.equal(winner.valueOf()[0], firstName);
+        assert.equal(winner.valueOf()[1], lastName);
+        assert.equal(web3.utils.hexToUtf8(winner.valueOf()[2].valueOf()), "+86 123456789");
     });
 });
