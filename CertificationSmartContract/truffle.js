8680d8d41c3fe7ed09a23f38ddae2ce89f49518e
Chaowei Zeng <sli616@163.com>
2020-09-17 09:57:36 +0800

    chore: update truffle networks

--- CertificationSmartContract/truffle.js
+++ CertificationSmartContract/truffle.js
@@ -1,34 +1,20 @@
-const argv = require('minimist')(process.argv.slice(2));
-const HDWalletProvider = require("truffle-hdwallet-provider");
-const infuraAccessToken = "fb032c29f8c94bf7aed00f42515ad21f";
-const provider = `https://ropsten.infura.io/v3/${infuraAccessToken}`;
+require('minimist')(process.argv.slice(2));
+require("truffle-hdwallet-provider");
 
 module.exports = {
 	networks: {
 		local: {
 			host: '127.0.0.1',
-			port: 8545,
+			port: 7545,
 			network_id: '*'
 		},
-		ropsten: {
-			network_id: 3,
-			provider: () => {
-				if (typeof argv.mnemonic === "undefined") {
-					throw new Error("Please provide mnemonic to deploy to ropsten!");
-				}
-				return new HDWalletProvider(argv.mnemonic, provider);
-			}
-		},
-		production: {
-			network_id: 3,
-			gas: 4465030,
-			provider: function() {
-				var WalletProvider = require("truffle-wallet-provider");
-				var keystore = require('fs').readFileSync('./keystore').toString();
-				var pass = require('fs').readFileSync('./pass').toString();
-				var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
-				return new WalletProvider(wallet, "https://ropsten.infura.io/")
-			}
+		quorum: {
+			network_id: "*",
+			host: "node1.quorum.cn.blockchain.thoughtworks.cn",
+			port: 80,
+			gasPrice: 0,
+			gas: 450000000,
+			type: "quorum"
 		}
 	}
 };
